d223d1ea6446ce814b834d0dec4ebbe97a207ac0
s27757 <s27757@pjwstk.edu.pl>
2024-03-20 15:11:32 +0100

    Task 8 - might need a redo

--- Task7/square_generator.py
+++ Task7/square_generator.py
@@ -21,3 +21,24 @@ try:
     print("Square roots:", square_roots)
 except ValueError as e:
     print(e)
+
+
+class CubicGenerator(SquareGenerator):
+    def generate_cubes(self, start, end):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return [i ** 3 for i in range(start, end + 1)]
+
+    def calculate_cube_roots(self, cubes):
+        return [math.pow(i, 1/3) for i in cubes]
+
+cubic_generator = CubicGenerator()
+
+try:
+    cubes = cubic_generator.generate_cubes(1, 10)
+    print("Cubes:", cubes)
+
+    cube_roots = cubic_generator.calculate_cube_roots(cubes)
+    print("Cube roots:", cube_roots)
+except ValueError as e:
+    print(e)
