6a8382ef55d07089bb48c1ad8d353ffffc0e1d3d
s27757 <s27757@pjwstk.edu.pl>
2024-03-20 15:14:23 +0100

    Task 9

--- square_generator.py
+++ square_generator.py
@@ -42,3 +42,46 @@ try:
     print("Cube roots:", cube_roots)
 except ValueError as e:
     print(e)
+
+
+
+""" Task 9 """
+
+import math
+
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return [i ** 2 for i in range(start, end + 1)]
+
+    def calculate_square_roots(self, squares):
+        return [math.sqrt(i) for i in squares]
+
+class CubicGenerator(SquareGenerator):
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return super().generate_squares(start, end)  # Call the parent class method
+
+    def generate_cubes(self, start, end):
+        if end < start:
+            raise ValueError("End of the range cannot be less than the start.")
+        return [i ** 3 for i in range(start, end + 1)]
+
+    def calculate_cube_roots(self, cubes):
+        return [math.pow(i, 1/3) for i in cubes]
+
+cubic_generator = CubicGenerator()
+
+try:
+    squares = cubic_generator.generate_squares(1, 10)
+    print("Squares:", squares)
+
+    cubes = cubic_generator.generate_cubes(1, 10)
+    print("Cubes:", cubes)
+
+    cube_roots = cubic_generator.calculate_cube_roots(cubes)
+    print("Cube roots:", cube_roots)
+except ValueError as e:
+    print(e)
